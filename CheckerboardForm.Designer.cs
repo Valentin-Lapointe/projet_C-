a1f22bf6af7aa971dacbd1bdef96683651f426a8
KatherineBreton <katherine.breton@viacesi.fr>
2019-11-20 15:37:52 +0100

    First Commit

---
+++ CheckerboardForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace CheckersClient
+{
+    partial class CheckerboardForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CheckerboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "CheckerboardForm";
+            this.Text = "Checkerboard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
