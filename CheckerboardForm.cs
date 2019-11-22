4d5b13609e72aad0cb52b793848094d8c209c761
CourelC <courel.charline@gmail.com>
2019-11-22 15:54:20 +0100

    feat Ajout du damier

--- CheckerboardForm.cs
+++ CheckerboardForm.cs
@@ -16,5 +16,37 @@ namespace CheckersClient
         {
             InitializeComponent();
         }
+
+        private void CheckerboardForm_Load(object sender, EventArgs e)
+        {
+            populateFLP();
+        }
+
+        private void populateFLP()
+        {
+            List<string> testNom = new List<string>();
+
+            Tile[] boardTiles = new Tile[100];
+            for (int i = 0; i < boardTiles.Length; i++)
+            {
+                boardTiles[i] = new Tile();
+                boardTiles[i].BackColor = System.Drawing.ColorTranslator.FromHtml("#a04f0e");
+                if (i % 2 == 0)
+                {
+                    if ((i >= 0 && i <= 9) || (i >= 20 && i <= 29) || (i >= 40 && i <= 49) || (i >= 60 && i <= 69) || (i >= 80 && i <= 89))
+                    {
+                        boardTiles[i].BackColor = System.Drawing.ColorTranslator.FromHtml("#ffe689");
+                    }
+                }
+                else if (i % 2 != 0)
+                {
+                    if ((i >= 10 && i <= 19) || (i >= 30 && i <= 39) || (i >= 50 && i <= 59) || (i >= 70 && i <= 79) || (i >= 90 && i <= 99))
+                    {
+                        boardTiles[i].BackColor = System.Drawing.ColorTranslator.FromHtml("#ffe689");
+                    }
+                }
+
+            }
+        }
     }
 }
