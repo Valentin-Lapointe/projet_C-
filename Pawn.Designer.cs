8acdec81d8dfb37ad6b68d6ebf701fa18528f71e
KatherineBreton <katherine.breton@viacesi.fr>
2019-12-17 08:42:37 +0100

    feat pawn

--- Pawn.Designer.cs
+++ Pawn.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "Pawn";
-            this.Size = new System.Drawing.Size(228, 238);
+            this.Size = new System.Drawing.Size(70, 70);
             this.ResumeLayout(false);
 
         }
