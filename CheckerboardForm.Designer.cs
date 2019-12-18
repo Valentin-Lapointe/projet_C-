5cbed121af2f68fa2c8c953eace3a7fa4fa7aa60
CourelC <courel.charline@gmail.com>
2019-12-18 13:36:16 +0100

    Init board

--- CheckerboardForm.Designer.cs
+++ CheckerboardForm.Designer.cs
@@ -3,14 +3,14 @@
     partial class CheckerboardForm
     {
         /// <summary>
-        /// Variable nécessaire au concepteur.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Nettoyage des ressources utilisées.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,29 +20,20 @@
             base.Dispose(disposing);
         }
 
-        #region Code généré par le Concepteur Windows Form
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
-        /// le contenu de cette méthode avec l'éditeur de code.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // CheckerboardForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.ImeMode = System.Windows.Forms.ImeMode.On;
-            this.Name = "CheckerboardForm";
-            this.Text = "Checkerboard";
-            this.ResumeLayout(false);
-
+            this.Text = "CheckerboardForm";
         }
 
         #endregion
     }
 }
-
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace CheckersClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CheckerboardForm());
+            Application.Run();
         }
     }
 }
